9d9f13e0a1748b2963dd814c744f08b7eccb7ae3
Sanjay Namdeo <sanjay.namdeo@live.com>
2019-11-03 19:53:36 +0100

    first commit

---
+++ src/main/java/com/learning/MathUtil.java
@@ -0,0 +1,7 @@
+package com.learning;
+
+public class MathUtil {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/com/learning/MathUtilTest.java
@@ -0,0 +1,15 @@
+package com.learning;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class MathUtilTest {
+    @Test
+    void test() {
+        MathUtil mathUtil = new MathUtil();
+        int expected = 3;
+        int actual = mathUtil.add(1, 1);
+        assertEquals(expected, actual);
+    }
+}
